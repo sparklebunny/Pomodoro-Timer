4a256f0fe44ea8bad78fdb9429e513aa87299be2
JeriBeth Stevens <jeribeth@gmail.com>
2017-02-08 18:22:51 -0500

    Added beginning code for FCC Comodoro timer app

---
+++ app.js
@@ -0,0 +1,27 @@
+//https://geeksretreat.wordpress.com/2012/08/03/html5-canvas-an-egg-timer-hourglass-with-animated-falling-sand/
+
+// break and timer:
+// click - to decrease min by 1 (0-5 on break; 0-25 on pomo)
+// click + to increase min by 1 (0-5 on break; 0-25 on pomo)
+// display amount of time on each
+// click Start to begin timer - one for break and one for timer
+// click stop to stop timers
+//click reset to clear timers to 0
+// each timer's display uses #countdown 
+// both timers cannot start at same time; only one timer can run at a time
+
+$(document).ready(function() {
+
+	$("#brdecrease").on("click", function(){
+		
+	});
+
+
+
+
+
+
+
+
+
+}); // FIN
