291e30b2e82da5dedaf78aeb64975aa1e35474c9
JeriBeth Stevens <jeribeth@gmail.com>
2017-02-09 20:59:22 -0500

    added button functionality +/-; rearranged HTML for timer to be on top of pg; updated CSS accordingly for timer display

--- app.js
+++ app.js
@@ -6,18 +6,56 @@
 // display amount of time on each
 // click Start to begin timer - one for break and one for timer
 // click stop to stop timers
-//click reset to clear timers to 0
+// click reset to clear timers to 0
 // each timer's display uses #countdown 
-// both timers cannot start at same time; only one timer can run at a time
+// countdown shown in min and seconds
+// both timers cannot start at same time; only one timer can run
 
 $(document).ready(function() {
 
+	var breakMinutes = 5;
+	var pomoMinutes = 25;
+	var breakStop = false;
+	var pomoStop = false;
+	var breakReset = "";
+	var pomoReset = "";
+	
+	// initializing the display to value of breakMinutes
+	$("#breakInput").val(breakMinutes);
+	// initializing the display to value of breakMinutes
+	$("#pomoInput").val(pomoMinutes);
+	
+	// decrease break by 1
 	$("#brdecrease").on("click", function(){
-		
+		if(breakMinutes > 0 && breakMinutes < 6) {
+			breakMinutes -= 1;
+			$("#breakInput").val(breakMinutes);
+		}
+	});
+	
+	//increase break by 1
+	$("#brincrease").on("click", function(){
+		if(breakMinutes >= 0 && breakMinutes < 5) {
+			breakMinutes += 1;
+			$("#breakInput").val(breakMinutes);
+		}
 	});
 
-
-
+	// decrease Pomo by 1
+	$("#pomominus").on("click", function(){
+		if(pomoMinutes > 0 && pomoMinutes < 26) {
+			pomoMinutes -= 1;
+			$("#pomoInput").val(pomoMinutes);
+		}
+	});
+	
+	//increase Pomo by 1
+	$("#pomoplus").on("click", function(){
+		if(pomoMinutes >= 0 && pomoMinutes < 25) {
+			pomoMinutes += 1;
+			$("#pomoInput").val(pomoMinutes);
+		}
+	});
 
 
 
