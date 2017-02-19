9c61761902e9fbf29bd3090658a5df4d67c9bae6
JeriBeth Stevens <jeribeth@gmail.com>
2017-02-18 21:43:23 -0500

    Completed js for timer - converted #s to minutes, seconds; added placeholder for <10; added buzzer and visual cue when timer's up, cleaned up HTML, JS

--- app.js
+++ app.js
@@ -1,5 +1,125 @@
 //https://geeksretreat.wordpress.com/2012/08/03/html5-canvas-an-egg-timer-hourglass-with-animated-falling-sand/
 
+$(document).ready(function() {
+
+	var breakMinutes = 5;
+	var pomoMinutes = 25;
+	var countdownInterval = null;
+	var buzzer = $("#buzzer")[0];
+	
+	//hide visual message for end of timer signal
+	$(".timeout_message_show").hide();
+	
+	// initializing the display to value of breakMinutes
+	$("#breakInput").val(breakMinutes);
+	
+	// initializing the display to value of breakMinutes
+	$("#pomoInput").val(pomoMinutes);
+	
+	// decrease break by 1
+	$("#brdecrease").on("click", function(){
+
+		if(breakMinutes > 0 && breakMinutes < 16) {
+
+			breakMinutes--;
+			$("#breakInput").val(breakMinutes);
+		}
+
+	});
+	
+	//increase break by 1
+	$("#brincrease").on("click", function(){
+
+		if(breakMinutes >= 0 && breakMinutes < 15) {
+
+			breakMinutes++;
+			$("#breakInput").val(breakMinutes);
+		}
+
+	});
+
+	// decrease Pomo by 1
+	$("#pomominus").on("click", function(){
+
+		if(pomoMinutes > 0 && pomoMinutes < 59) {
+			pomoMinutes--;
+			$("#pomoInput").val(pomoMinutes);
+		}
+
+		//display current value on large timer
+		document.querySelector("#countdownTime").innerHTML = pomoMinutes;
+
+	});
+	
+	//increase Pomo by 1
+	$("#pomoplus").on("click", function(){
+
+		if(pomoMinutes >= 0 && pomoMinutes < 59) {
+
+			pomoMinutes++;
+			$("#pomoInput").val(pomoMinutes);
+		}
+
+		//display current value on large timer
+		document.querySelector("#countdownTime").innerHTML = pomoMinutes;
+
+	});
+
+	// function for determining countdown time
+	function countDown(time) {
+		
+		//convert number passed to CountDown function to seconds
+		var countdownSeconds = time * 60;
+		
+		// convert time to minutes and seconds, rounding with Math.floor to
+		//eliminate weird decimal stuff from happening
+		countdownInterval = setInterval(function(){
+			
+			var pomoMinutes = Math.floor(countdownSeconds / 60);
+			var pomoSeconds = Math.floor(countdownSeconds % 60);
+			
+			pomoMinutes = pomoMinutes < 10 ? "0" + pomoMinutes : pomoMinutes;
+			pomoSeconds = pomoSeconds < 10 ? "0" + pomoSeconds : pomoSeconds;
+			
+			if (countdownSeconds === 0) {
+				buzzer.play();
+				$(".timeout_message_show").show();
+				$("#countdownTime").hide();
+			}
+			
+			var displayTime = pomoMinutes + ":" + pomoSeconds;
+			document.getElementById("countdownTime").innerHTML = displayTime;
+			countdownSeconds-- || clearInterval(countdownInterval);
+		}, 1000)
+	
+	}
+
+	// Starts the pomodoro clock
+	var startPomoClock = $("#start").on("click", function(){
+
+		countDown(pomoMinutes);
+
+	});
+
+	// Stops clock on current min/sec
+	var stopPomoClock = $("#stop").on("click", function(){
+		
+		clearTimeout(countdownInterval);	
+	});
+ 
+ 	// Resets clock to time/number shown on pomodoro input
+	var resetPomoClock = $("#reset").on("click", function(){
+		
+		clearTimeout(countdownInterval);	
+
+		var currentPomoMinutes = (document.querySelector("#countdownTime").innerHTML = pomoMinutes);
+	});
+		
+
+}); // FIN
+
+////// NOTES BELOW
+
 // break and timer:
 // DONE: click - to decrease min by 1 (0-5 on break; 0-25 on pomo) 
 // DONE: click + to increase min by 1 (0-5 on break; 0-25 on pomo)
@@ -12,117 +132,11 @@
 // each timer's display uses #countdown 
 // countdown shown in min and seconds
 // both timers cannot start at same time; only one timer can run
-
-$(document).ready(function() {
-
-	var breakMinutes = 5;
-	var pomoMinutes = 25;
-	
-	
-	// initializing the display to value of breakMinutes
-	$("#breakInput").val(breakMinutes);
-	// initializing the display to value of breakMinutes
-	$("#pomoInput").val(pomoMinutes);
-	
-
-	// decrease break by 1
-	$("#brdecrease").on("click", function(){
-		if(breakMinutes > 0 && breakMinutes < 16) {
-			breakMinutes--;
-			$("#breakInput").val(breakMinutes);
-		}
-	});
-	
-	//increase break by 1
-	$("#brincrease").on("click", function(){
-		if(breakMinutes >= 0 && breakMinutes < 15) {
-			breakMinutes++;
-			$("#breakInput").val(breakMinutes);
-		}
-	});
-
-	// decrease Pomo by 1
-	$("#pomominus").on("click", function(){
-		if(pomoMinutes > 0 && pomoMinutes < 59) {
-			pomoMinutes--;
-			$("#pomoInput").val(pomoMinutes);
-		}
-
-		//display current value on large timer
-		document.querySelector("#countdownTime").innerHTML = pomoMinutes;
-	});
-	
-	//increase Pomo by 1
-	$("#pomoplus").on("click", function(){
-		if(pomoMinutes >= 0 && pomoMinutes < 59) {
-			pomoMinutes++;
-			$("#pomoInput").val(pomoMinutes);
-		}
-
-		//display current value on large timer
-		document.querySelector("#countdownTime").innerHTML = pomoMinutes;
-	});
-
-
-
-	function countDown(time) {
-
-		var interval = setInterval(function(){
-			document.getElementById("countdownTime").innerHTML = time;
-			time-- || clearInterval(interval);
-		}, 1000)
-	}
-
-	//starts the pomodoro clock
-	var startPomoClock = $("#start").on("click", function(){
-
-		countDown(pomoMinutes);
-
-	});
-///
-/// Need this to work
-	var stopPomoClock = $("#stop").on("click", function(){
-
-		var timer = setTimeout(countDown, 1000);
-		clearTimeout(timer);		
-	});
-///
-/// Need this to work
-	var resetPomoClock = $("#reset").on("click", function(){
-	
-		var currentPomoMinutes = (document.querySelector("#countdownTime").innerHTML = pomoMinutes);
-
-		countDown(currentPomoMinutes);
-
-	});
-		
-
-
-
-
-	// converting minutes to min/seconds
-
-
-
-
-
-////// NOTES BELOW
-
-
-//var countdownSeconds = 0; or countdownMinutes 
-
-
-
-
-}); // FIN
-
 // state: is timer running or not
 // when pomo up - switch to break time
 // is pomo running?
 // is break running?
 // both can't run at same time 
-
-
 
 // action: click start btn to start timer
 // set interval of pomodoro
